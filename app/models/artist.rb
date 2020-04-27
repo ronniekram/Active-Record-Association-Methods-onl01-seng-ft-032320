8c557590436aa58d50fe0059aa75b7688b57b0b5
Arynn Boniface <arynn.boniface@gmail.com>
2020-04-27 00:20:08 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,14 +3,14 @@ class Artist < ActiveRecord::Base
   has_many :genres, through: :songs
 
   def get_genre_of_first_song
-    #return the genre of the artist's first saved song
+    Artist.genre.first
   end
 
   def song_count
-    #return the number of songs associated with the artist
+    Artist.song.count
   end
 
   def genre_count
-    #return the number of genres associated with the artist
+    Artist.genre.count
   end
 end
